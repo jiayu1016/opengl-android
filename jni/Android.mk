e9ae02fd75b2585a7f86685e0e334668f082bd60
Simon Racz <simonracz@gmail.com>
2013-08-27 21:23:01 +0200

    Setting up makefiles. Exported car mesh from Blender.

--- jni/Android.mk
+++ jni/Android.mk
@@ -3,6 +3,10 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE    := opengl-android
-LOCAL_SRC_FILES := opengl-android.cpp
+LOCAL_SRC_FILES := main.cpp
+LOCAL_LDLIBS    := -llog -landroid -lEGL -lGLESv2
+LOCAL_STATIC_LIBRARIES := android_native_app_glue
 
 include $(BUILD_SHARED_LIBRARY)
+
+$(call import-module,android/native_app_glue)
---
+++ jni/main.cpp
@@ -0,0 +1,12 @@
+#include <jni.h>
+
+#include <android/log.h>
+
+#include <android_native_app_glue.h>
+
+
+
+void android_main(struct android_app* app)
+{
+
+}
